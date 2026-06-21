5592453e98d6509f5823f86d3872271ff969b7c5
U-CORP\dmitry.bublikov <dmitry.bublikov@CPR-C223092546.ax-team.corp>
2026-06-21 14:21:32 +0300

    fix2 for program import

--- 5_Structure_import_to_CIF_format/src/Import_to_cif.cpp
+++ 5_Structure_import_to_CIF_format/src/Import_to_cif.cpp
@@ -15,7 +15,6 @@
 
 //===========================================================================
 // FUNCTION: gcd
-// Description: Greatest common divisor (Euclidean algorithm)
 //===========================================================================
 int gcd(int a, int b) {
     while (b != 0) {
@@ -28,7 +27,6 @@ int gcd(int a, int b) {
 
 //===========================================================================
 // STRUCT: CationProperties
-// Description: Stores all properties of a cation
 //===========================================================================
 struct CationProperties {
     std::string symbol;
@@ -38,7 +36,6 @@ struct CationProperties {
 
 //===========================================================================
 // STRUCT: AnionProperties
-// Description: Stores properties of anions (oxygen)
 //===========================================================================
 struct AnionProperties {
     std::string symbol;
@@ -48,7 +45,6 @@ struct AnionProperties {
 
 //===========================================================================
 // STRUCT: AnionSite
-// Description: Stores all CIF parameters for an anion site
 //===========================================================================
 struct AnionSite {
     int label;
@@ -63,67 +59,7 @@ struct AnionSite {
 };
 
 //===========================================================================
-// FUNCTION: load_anion_properties
-// Description: Loads anion properties from file
-// File format: "<code> <symbol> <charge> <atomic_weight> <radius>"
-// Example: "1 Cl 1 35.453 1.81"  (charge is absolute value, stored as negative)
-//===========================================================================
-std::map<int, AnionProperties> load_anion_properties(const std::string& filename) {
-    std::map<int, AnionProperties> anions;
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Error: Cannot open " << filename << std::endl;
-        return anions;
-    }
-    
-    int code;
-    std::string symbol;
-    int charge;
-    double atomic_weight;
-
-    while (file >> code >> symbol >> charge >> atomic_weight) {
-        AnionProperties props;
-        props.symbol = symbol;
-        props.charge = -charge;
-        props.atomic_weight = atomic_weight;
-        anions[code] = props;
-    }
-    
-    return anions;
-}
-
-//===========================================================================
-// FUNCTION: get_anion_symbol
-// Description: Returns element symbol for a given anion code
-//===========================================================================
-std::string get_anion_symbol(int code, const std::map<int, AnionProperties>& anions) {
-    std::map<int, AnionProperties>::const_iterator it = anions.find(code);
-    if (it != anions.end()) {
-        return it->second.symbol;
-    }
-    return "X";
-}
-
-//===========================================================================
-// FUNCTION: get_anion_properties_by_code
-// Description: Returns anion properties for a given code
-//===========================================================================
-AnionProperties get_anion_properties_by_code(int code, const std::map<int, AnionProperties>& anions) {
-    std::map<int, AnionProperties>::const_iterator it = anions.find(code);
-    if (it != anions.end()) {
-        return it->second;
-    }
-    // Return default value
-    AnionProperties default_props;
-    default_props.symbol = "O";
-    default_props.charge = -2;
-    default_props.atomic_weight = 15.9990;
-    return default_props;
-}
-
-//===========================================================================
 // STRUCT: CationSite
-// Description: Stores all CIF parameters for a cation site
 //===========================================================================
 struct CationSite {
     int label;
@@ -139,9 +75,6 @@ struct CationSite {
 
 //===========================================================================
 // FUNCTION: load_cation_properties
-// Description: Loads cation properties from file
-// File format: "<code> <symbol> <charge> <atomic_weight> <radius>"
-// Example: "1 Na 1 22.9898 1.02"
 //===========================================================================
 std::map<int, CationProperties> load_cation_properties(const std::string& filename) {
     std::map<int, CationProperties> cations;
@@ -155,7 +88,7 @@ std::map<int, CationProperties> load_cation_properties(const std::string& filena
     std::string symbol;
     int charge;
     double atomic_weight;
-
+    
     while (file >> code >> symbol >> charge >> atomic_weight) {
         CationProperties props;
         props.symbol = symbol;
@@ -168,39 +101,49 @@ std::map<int, CationProperties> load_cation_properties(const std::string& filena
 }
 
 //===========================================================================
-// FUNCTION: load_anion_sites
-// Description: Loads anion sites with all CIF parameters from file
-// File format: "<label> <x> <y> <z> <occupancy> <mult> <wyckoff> <H> <calc> <disp> <u_iso>"
-// Example: "1 0.0 0.0 0.0 1.000 1 a ? d ? ?"
+// FUNCTION: load_anion_properties
 //===========================================================================
-std::vector<AnionSite> load_anion_sites(const std::string& filename) {
-    std::vector<AnionSite> sites;
+std::map<int, AnionProperties> load_anion_properties(const std::string& filename) {
+    std::map<int, AnionProperties> anions;
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error: Cannot open " << filename << std::endl;
-        return sites;
+        return anions;
     }
     
-    AnionSite site;
-    while (file >> site.label 
-           >> site.x >> site.y >> site.z 
-           >> site.occupancy >> site.multiplicity 
-           >> site.wyckoff >> site.hydrogens 
-           >> site.calc_flag >> site.displace_type 
-           >> site.u_iso) {
-        sites.push_back(site);
+    int code;
+    std::string symbol;
+    int charge;
+    double atomic_weight;
+    
+    while (file >> code >> symbol >> charge >> atomic_weight) {
+        AnionProperties props;
+        props.symbol = symbol;
+        props.charge = -charge;
+        props.atomic_weight = atomic_weight;
+        anions[code] = props;
     }
     
-    if (sites.size() != 32) {
-        std::cerr << "Warning: Expected 32 anion sites, got " << sites.size() << std::endl;
+    return anions;
+}
+
+//===========================================================================
+// FUNCTION: get_anion_properties_by_code
+//===========================================================================
+AnionProperties get_anion_properties_by_code(int code, const std::map<int, AnionProperties>& anions) {
+    std::map<int, AnionProperties>::const_iterator it = anions.find(code);
+    if (it != anions.end()) {
+        return it->second;
     }
-    
-    return sites;
+    AnionProperties default_props;
+    default_props.symbol = "O";
+    default_props.charge = -2;
+    default_props.atomic_weight = 15.9990;
+    return default_props;
 }
 
 //===========================================================================
 // FUNCTION: load_ordering
-// Description: Loads 32 cation type codes from file
 //===========================================================================
 std::vector<int> load_ordering(const std::string& filename) {
     std::vector<int> ordering;
@@ -224,9 +167,6 @@ std::vector<int> load_ordering(const std::string& filename) {
 
 //===========================================================================
 // FUNCTION: load_cation_sites
-// Description: Loads cation sites with all CIF parameters from file
-// File format: "<label> <x> <y> <z> <occupancy> <mult> <wyckoff> <H> <calc> <disp> <u_iso>"
-// Example: "1 0.2500 0.2500 0.2500 1.000 1 a ? d ? ?"
 //===========================================================================
 std::vector<CationSite> load_cation_sites(const std::string& filename) {
     std::vector<CationSite> sites;
@@ -254,33 +194,35 @@ std::vector<CationSite> load_cation_sites(const std::string& filename) {
 }
 
 //===========================================================================
-// FUNCTION: load_anion_positions
-// Description: Loads 32 anion positions with fractional coordinates
+// FUNCTION: load_anion_sites
 //===========================================================================
-std::vector<std::tuple<double, double, double>> load_anion_positions(const std::string& filename) {
-    std::vector<std::tuple<double, double, double>> positions;
+std::vector<AnionSite> load_anion_sites(const std::string& filename) {
+    std::vector<AnionSite> sites;
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cerr << "Error: Cannot open " << filename << std::endl;
-        return positions;
+        return sites;
     }
     
-    int label;
-    double x, y, z;
-    while (file >> label >> x >> y >> z) {
-        positions.push_back({x, y, z});
+    AnionSite site;
+    while (file >> site.label 
+           >> site.x >> site.y >> site.z 
+           >> site.occupancy >> site.multiplicity 
+           >> site.wyckoff >> site.hydrogens 
+           >> site.calc_flag >> site.displace_type 
+           >> site.u_iso) {
+        sites.push_back(site);
     }
     
-    if (positions.size() != 32) {
-        std::cerr << "Warning: Expected 32 anion positions, got " << positions.size() << std::endl;
+    if (sites.size() != 32) {
+        std::cerr << "Warning: Expected 32 anion sites, got " << sites.size() << std::endl;
     }
     
-    return positions;
+    return sites;
 }
 
 //===========================================================================
 // FUNCTION: get_cation_symbol
-// Description: Returns element symbol for a given type code
 //===========================================================================
 std::string get_cation_symbol(int code, const std::map<int, CationProperties>& cations) {
     std::map<int, CationProperties>::const_iterator it = cations.find(code);
@@ -292,7 +234,6 @@ std::string get_cation_symbol(int code, const std::map<int, CationProperties>& c
 
 //===========================================================================
 // FUNCTION: get_cation_charge
-// Description: Returns charge for a given type code
 //===========================================================================
 int get_cation_charge(int code, const std::map<int, CationProperties>& cations) {
     std::map<int, CationProperties>::const_iterator it = cations.find(code);
@@ -304,7 +245,6 @@ int get_cation_charge(int code, const std::map<int, CationProperties>& cations) 
 
 //===========================================================================
 // FUNCTION: get_cation_weight
-// Description: Returns atomic weight for a given type code
 //===========================================================================
 double get_cation_weight(int code, const std::map<int, CationProperties>& cations) {
     std::map<int, CationProperties>::const_iterator it = cations.find(code);
@@ -316,15 +256,6 @@ double get_cation_weight(int code, const std::map<int, CationProperties>& cation
 
 //===========================================================================
 // FUNCTION: calculate_formula
-// Description: Calculates chemical formula with:
-//              1. Cations sorted by charge (ascending: Na⁺, Mg²⁺, Fe³⁺)
-//              2. Vacancies [] after cations, before oxygen
-//              3. Oxygen (anions) last
-//              4. Reduced coefficients (gcd)
-//===========================================================================
-//===========================================================================
-// FUNCTION: calculate_formula
-// Description: Calculates chemical formula with anions from file
 //===========================================================================
 std::string calculate_formula(const std::vector<int>& ordering, 
                               const std::map<int, CationProperties>& cations,
@@ -334,7 +265,6 @@ std::string calculate_formula(const std::vector<int>& ordering,
     std::map<int, std::string> charge_symbol;
     int vacancy_count = 0;
     
-    // Collect cation data
     for (int i = 0; i < (int)ordering.size(); i++) {
         int code = ordering[i];
         if (code == 0) {
@@ -349,23 +279,19 @@ std::string calculate_formula(const std::vector<int>& ordering,
         }
     }
     
-    // Add anion (will be last)
     charge_counts[anion_props.charge] = anion_count;
     charge_symbol[anion_props.charge] = anion_props.symbol;
     
-    // Add vacancies (will be before anion)
     if (vacancy_count > 0) {
         charge_counts[0] = vacancy_count;
     }
     
-    // Sort charges in ascending order
     std::vector<int> charges;
     for (std::map<int, int>::iterator it = charge_counts.begin(); it != charge_counts.end(); ++it) {
         charges.push_back(it->first);
     }
     std::sort(charges.begin(), charges.end());
     
-    // Find GCD for reduction
     int g = 0;
     for (std::map<int, int>::iterator it = charge_counts.begin(); it != charge_counts.end(); ++it) {
         if (it->second > 0) {
@@ -374,10 +300,8 @@ std::string calculate_formula(const std::vector<int>& ordering,
     }
     if (g < 1) g = 1;
     
-    // Build formula
     std::string formula;
     
-    // 1. Cations (positive charges)
     for (std::vector<int>::iterator it = charges.begin(); it != charges.end(); ++it) {
         int charge = *it;
         if (charge > 0) {
@@ -391,7 +315,6 @@ std::string calculate_formula(const std::vector<int>& ordering,
         }
     }
     
-    // 2. Vacancies
     if (vacancy_count > 0) {
         int count = charge_counts[0] / g;
         if (count > 1) {
@@ -401,7 +324,6 @@ std::string calculate_formula(const std::vector<int>& ordering,
         }
     }
     
-    // 3. Anion (negative charge) — always last
     for (std::vector<int>::iterator it = charges.begin(); it != charges.end(); ++it) {
         int charge = *it;
         if (charge < 0) {
@@ -421,7 +343,6 @@ std::string calculate_formula(const std::vector<int>& ordering,
 
 //===========================================================================
 // FUNCTION: calculate_molecular_weight
-// Description: Calculates molecular weight from ordering
 //===========================================================================
 double calculate_molecular_weight(const std::vector<int>& ordering,
                                   const std::map<int, CationProperties>& cations,
@@ -432,31 +353,14 @@ double calculate_molecular_weight(const std::vector<int>& ordering,
         int code = ordering[i];
         weight += get_cation_weight(code, cations);
     }
-    // Add anion weight
     weight += anion_count * anion_props.atomic_weight;
     return weight;
-}
-
-//===========================================================================
-// FUNCTION: get_anion_properties
-// Description: Returns properties of oxygen anion
-//===========================================================================
-AnionProperties get_anion_properties() {
-    AnionProperties o;
-    o.symbol = "O";
-    o.charge = -2;
-    o.atomic_weight = 15.9990;
-    return o;
 }
 
 //===========================================================================
 // MAIN FUNCTION
 //===========================================================================
 int main() {
-    //=======================================================================
-    // 1. LOAD DATA FROM FILES
-    //=======================================================================
-    
     // Load cations
     std::map<int, CationProperties> cations = load_cation_properties("types_cations.txt");
     if (cations.empty()) {
@@ -492,22 +396,14 @@ int main() {
         return 1;
     }
     
-    // Anion code (select the desired anion)
-    int anion_code = 1;  // if anion is number 1 from types_anions.txt
-    	// Or can ask the user:
-    // std::cout << "Enter anion code (1-Cl, 2-O, 3-N): ";
-    // std::cin >> anion_code;
-    
+    // Anion code
+    int anion_code = 1;
     AnionProperties anion_props = get_anion_properties_by_code(anion_code, anions);
     
-    //=======================================================================
-    // 2. GENERATE FULL CIF FILE
-    //=======================================================================
-    
+    // Generate CIF
     std::ofstream cif("output.cif");
     cif << std::fixed << std::setprecision(4);
     
-    // --- CIF Header ---
     cif << "data_CF32\n";
     cif << "_audit_creation_method 'Cleaned and symmetry-consistent CIF'\n";
     cif << "_audit_creation_date\t2026-05-04\n";
@@ -519,7 +415,6 @@ int main() {
     double weight = calculate_molecular_weight(ordering, cations, anion_props, 32);
     cif << "_chemical_formula_weight\t" << std::fixed << std::setprecision(3) << weight << "\n";
     
-    // Cell parameters
     cif << "_cell_length_a\t8.0000\n";
     cif << "_cell_length_b\t8.0000\n";
     cif << "_cell_length_c\t8.0000\n";
@@ -528,19 +423,16 @@ int main() {
     cif << "_cell_angle_gamma\t90.000\n";
     cif << "_cell_volume\t512.0\n";
     
-    // Symmetry
     cif << "_symmetry_cell_setting\ttriclinic\n";
     cif << "_symmetry_int_tables_number\t1\n";
     cif << "_symmetry_space_group_name_H-M\t'P 1'\n";
     cif << "_symmetry_space_group_name_Hall\t'P_1'\n\n";
     
-    // Symmetry operations
     cif << "loop_\n";
     cif << "_symmetry_equiv_pos_site_id\n";
     cif << "_symmetry_equiv_pos_as_xyz\n";
     cif << "1 x,y,z\n\n";
     
-    // Atom types
     std::map<std::string, bool> added_types;
     
     cif << "loop_\n";
@@ -548,7 +440,6 @@ int main() {
     cif << "_atom_type_oxidation_number\n";
     cif << "_atom_type_radius_bond\n";
     
-    // Add cation types
     for (int i = 0; i < (int)ordering.size(); i++) {
         int code = ordering[i];
         std::string sym = get_cation_symbol(code, cations);
@@ -565,7 +456,6 @@ int main() {
     }
     cif << "\n";
     
-    // Atom sites loop
     cif << "loop_\n";
     cif << "_atom_site_label\n";
     cif << "_atom_site_type_symbol\n";
@@ -580,19 +470,11 @@ int main() {
     cif << "_atom_site_thermal_displace_type\n";
     cif << "_atom_site_u_iso_or_equiv\n";
     
-    // Write cations (skip vacancies - code 0, but keep original numbering)
     for (std::vector<CationSite>::iterator it = cation_sites.begin();
          it != cation_sites.end(); ++it) {
-    
         int code = ordering[it->label - 1];
-    
-        // Check: if code == 0 (vacancy) - skip entry
-        if (code == 0) {
-            continue;  // Move to next position, but label C remains unchanged
-        }
-    
+        if (code == 0) continue;
         std::string sym = get_cation_symbol(code, cations);
-    
         cif << "C" << it->label << " "
             << sym << "  "
             << std::fixed << std::setprecision(4) << it->x << "  "
@@ -607,7 +489,6 @@ int main() {
             << it->u_iso << "\n";
     }
     
-    // Write anions
     for (std::vector<AnionSite>::iterator it = anion_sites.begin();
          it != anion_sites.end(); ++it) {
         cif << anion_props.symbol << it->label << " "
